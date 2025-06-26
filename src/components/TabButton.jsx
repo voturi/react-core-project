078eba959cebefbf53773ca8b4dd4154f286d4f5
voturi <praveen.voturi@service.nsw.gov.au>
2025-06-26 15:59:40 +1000

    feature/project init

---
+++ src/components/TabButton.jsx
@@ -0,0 +1,6 @@
+export default function TabButton({children, onSelect, isSelected}){
+
+    return (
+         <li> <button className = {isSelected && 'active'} onClick = {onSelect} >{children}</button></li>
+    )
+}
